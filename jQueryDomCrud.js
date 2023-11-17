04f48dcccace97452e3ed9c50885a8c9e928287b
FlorentinPopescu <popescu01@gmail.com>
2023-11-16 23:36:46 -0600

    Lesson_06

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,40 @@
+$(document).ready(function() {
+
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
 
+let buyEl = $("<a></a>").text("Buy Now!"); 
+$("p").after(buyEl);
+$(buyEl).attr("id", "cta");
+
 // Access (read) the data-color attribute of the <img>,
 // log to the console
+let colorImg = $("img").attr("data-color");
+console.log(colorImg);
 
 // Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
 
+let listItems = $("ul li");
+for (let i = 0; i <= listItems.length; i++) {
+    if($(listItems[i]).text() == "Turbocharged") {
+        $(listItems[i]).attr("class", "highlight");
+    }
+}
+
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+
+$("p").last().remove();
 
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
 // and replaced with text that says "Added to cart"
+
+buyEl.on( "click", function() {
+    buyEl.remove();
+    let addToCard = $("<p></p>").text("Added to cart");
+    $("ul").after(addToCard);
+  });
+
+});
--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -3,8 +3,46 @@ document.getElementById('cat').addEventListener('click', () => {
   alert('meow!');
 });
 
+
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+/* ------- jQuery -----------*/ 
+let moreInfo = $("#more-info").removeAttr("href");
+moreInfo.on( "click", (e) => {
+  e.preventDefault();
+  alert("Here's some info");
+  e.stopPropagation();
+});
+
+/* ------- Javascript -------*/
+/*
+document.getElementById("more-info").removeAttribute('href');
+let moreInfo = document.getElementById("more-info");
+
+moreInfo.addEventListener('click', (e) => {
+  e.preventDefault();
+  alert("Here's some info");
+  e.stopPropagation();
+}, false);
+*/
+
+
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+/* ------- jQuery -----------*/
+$("#dog").on("click", (e) => {
+  e.preventDefault();
+  alert('Bow wow!');
+  e.stopPropagation();
+});
+
+/* ------- Javascript -------*/
+/*
+document.getElementById('dog').addEventListener('click', (e) => {
+  e.preventDefault();
+  alert('Bow wow!');
+  e.stopPropagation();
+});
+*/
